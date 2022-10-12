f00513705853c7192ff62ec00483cbdc04840e3a
Magnus Hodne <magnus@drobak.org>
2022-10-12 15:07:20 +0200

    Added URLs to Auth0

--- client/auth0.config.tsx
+++ client/auth0.config.tsx
@@ -1,12 +1,30 @@
 import { Auth0Provider } from "@auth0/auth0-react";
 import * as React from "react";
 
-export function Auth0Config({ children }: { children: React.ReactNode | React.ReactNode[] }) {
+export function Auth0Config({
+  children,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+}) {
+  const domain = "https://magnushodne.eu.auth0.com/";
+  const clientId = "JN8jmWPtBrI52sVNAAP5ZHntM4VKAc3L";
+  let redirectUri = "https://dndnotes.trovik.dev/callback";
+  let audience = "https://api.trovik.dev";
+
+  if (process.env.NODE_ENV === "development") {
+    redirectUri = "http://localhost:3000/callback";
+    audience = "http://localhost:5000";
+  }
+
+  if (!(domain && clientId && redirectUri && audience)) {
+    return null;
+  }
+
   return (
     <Auth0Provider
-      domain={process.env.REACT_APP_AUTH0_DOMAIN}
-      clientId={process.env.REACT_APP_AUTH0_CLIENT_ID}
-      redirectUri={window.location.origin}
+      domain={domain}
+      clientId={clientId}
+      redirectUri={redirectUri}
     >
       {children}
     </Auth0Provider>

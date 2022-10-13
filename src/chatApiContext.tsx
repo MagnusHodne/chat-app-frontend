5ca8fe69de9f474031d6cb789cc592c4fd7a0782
Magnus H <me@magnushodne.com>
2022-10-13 20:12:43 +0200

    Changed endpoint urls

--- src/chatApiContext.tsx
+++ src/chatApiContext.tsx
@@ -4,15 +4,15 @@ import { deleteJSON, fetchJSON, putJSON } from "./lib/fetchUtils";
 export const ChatApiContext = React.createContext({
   /*==== DATABASE OPERATIONS =====*/
   async fetchChatLog() {
-    return await fetchJSON("/api/chat");
+    return await fetchJSON("/api/v1/chat");
   },
   async fetchUserInfo({ sub }: { sub: string }) {
-    return await fetchJSON(`/api/user?sub=${sub}`);
+    return await fetchJSON(`/api/v1/user?sub=${sub}`);
   },
   async updateUserBio({ sub, bio }: { sub: string; bio: string }) {
-    return await putJSON("/api/user", { sub, bio });
+    return await putJSON("/api/v1/user", { sub, bio });
   },
   async deleteMessage({ _id }: { _id: any }) {
-    return await deleteJSON("/api/chat", { _id });
+    return await deleteJSON("/api/v1/chats", { _id });
   },
 });

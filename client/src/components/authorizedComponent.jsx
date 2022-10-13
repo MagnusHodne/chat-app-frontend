0fff5c87746941e82f8eb192418dd67cd32cd2a4
Magnus H <me@magnushodne.com>
2022-10-13 16:56:45 +0200

    Rewrote authorizedComponent.jsx to use Auth0's hook

--- client/src/components/authorizedComponent.jsx
+++ client/src/components/authorizedComponent.jsx
@@ -1,33 +1,11 @@
-import React, { useContext, useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from "react";
 import { LoadingComponent } from "./feedbackComponents";
-import { ChatApiContext } from "../chatApiContext";
-import { UserContext } from "../userContext";
+import { withAuthenticationRequired } from "@auth0/auth0-react";
 
-/**
- * Simple wrapper for components that require the user to be authorized
- * @param user the user object used for checking verification
- * @param {boolean} writeAccess Whether this component needs write access (default is false)
- * @param successComponent
- * @param failComponent
- */
-export function AuthorizedComponent({
-  writeAccess = false,
-  successComponent = <div>Success</div>,
-  failComponent = <div>Fail</div>,
-}) {
-  const navigate = useNavigate();
-  const [verified, setVerified] = useState(false);
-  const { user } = useContext(UserContext);
-  const { verifyUser } = useContext(ChatApiContext);
-  useEffect(() => {
-    if (!verifyUser(user)) {
-      navigate("/startlogin");
-    } else {
-      setVerified(true);
-    }
-  }, []);
-  if (!verified)
-    return <LoadingComponent message={"Redirecting to login..."} />;
-  return successComponent;
-}
+// Wrapper class for Auth0's withAuthenticationRequired hook
+const AuthorizedComponent = ({ children }) => <>{children}</>;
+export default withAuthenticationRequired(AuthorizedComponent, {
+  onRedirecting: () => (
+    <LoadingComponent message={"Redirecting to login page..."} />
+  ),
+});
--- client/src/pages/frontPage.jsx
+++ client/src/pages/frontPage.jsx
@@ -5,7 +5,7 @@ import { ProfilePage } from "./profilePage";
 import { ChatPage } from "./chatPage";
 import { NotFoundPage } from "./notFoundPage";
 import { Button } from "../components/basics";
-import { AuthorizedComponent } from "../components/authorizedComponent";
+import AuthorizedComponent from "../components/authorizedComponent";
 
 export function FrontPage() {
   function ContentRouter() {
@@ -27,13 +27,11 @@ export function FrontPage() {
     );
   }
   return (
-    <AuthorizedComponent
-      successComponent={
-        <GenericPage
-          mainContent={<ContentRouter />}
-          sidebarContent={<SidebarContent />}
-        ></GenericPage>
-      }
-    />
+    <AuthorizedComponent>
+      <GenericPage
+        mainContent={<ContentRouter />}
+        sidebarContent={<SidebarContent />}
+      ></GenericPage>
+    </AuthorizedComponent>
   );
 }

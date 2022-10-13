f6c35d45ac0c9f131ed2c44fd4867b19ece3af30
Magnus H <me@magnushodne.com>
2022-10-13 17:05:03 +0200

    Updated authorizedComponent to TS

--- client/src/components/authorizedComponent.tsx
+++ client/src/components/authorizedComponent.tsx
@@ -3,7 +3,12 @@ import { LoadingComponent } from "./feedbackComponents";
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
 // Wrapper class for Auth0's withAuthenticationRequired hook
-const AuthorizedComponent = ({ children }) => <>{children}</>;
+const AuthorizedComponent = ({
+  children,
+}: {
+  children: React.ReactNode | React.ReactNode[];
+}) => <>{children}</>;
+
 export default withAuthenticationRequired(AuthorizedComponent, {
   onRedirecting: () => (
     <LoadingComponent message={"Redirecting to login page..."} />

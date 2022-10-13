a26127d065cb47209c03f34e892996b229135a51
Magnus H <me@magnushodne.com>
2022-10-13 17:12:23 +0200

    Converted UserContext to TS

--- client/src/userContext.tsx
+++ client/src/userContext.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const UserContext = React.createContext({
   user: {},
-  verifyUser(user) {
+  verifyUser(user: any) {
     if (!user) return false;
     return Object.entries(user).length !== 0;
   },

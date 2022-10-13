d80331514543f405f0106fe3c4de3b46d01fdb77
Magnus H <me@magnushodne.com>
2022-10-13 16:54:12 +0200

    Removed express server code

--- server/model/Message.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const messageSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  created: { type: Date, default: Date.now, required: true, immutable: true },
-  message: String,
-});
-
-export const Message = mongoose.model("Message", messageSchema);

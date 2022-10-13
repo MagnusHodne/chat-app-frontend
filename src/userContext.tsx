972524fc50f336d731634921ccd7b3288e240719
Magnus H <me@magnushodne.com>
2022-10-13 18:08:08 +0200

    Removed verifyUser method

--- src/userContext.tsx
+++ src/userContext.tsx
@@ -2,8 +2,4 @@ import React from "react";
 
 export const UserContext = React.createContext({
   user: {},
-  verifyUser(user: any) {
-    if (!user) return false;
-    return Object.entries(user).length !== 0;
-  },
 });
